65afa73513bcf50759dd03e9ecb9b17968841788
Rakoski <mastrakoski@gmail.com>
2025-04-27 13:22:39 -0300

    first commit

--- src/ImplementStackUsingQueue.java
+++ src/ImplementStackUsingQueue.java
@@ -1,3 +1,34 @@
-public class ImplementStackUsingQueue {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class ImplementStackUsingQueue {
+    private Queue<Integer> queue;
+    private Queue<Integer> queue2; // helper q
+
+    public ImplementStackUsingQueue() {
+        queue = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        queue2.offer(x);
+        while (!queue.isEmpty()) {
+            queue2.offer(queue.poll()); // vai dando um por um até queue ficar vazia
+        }
+        Queue<Integer> temp = queue;
+        queue = queue2;
+        queue2 = temp;
+    }
+
+    public void pop() {
+        queue.poll();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
 }
